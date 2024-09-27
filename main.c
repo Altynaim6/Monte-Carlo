6a3a4a1910ebce4e1cf5d81f125c2e9587932cb6
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-09-27 15:31:40 +0600

    changed to monte carlo

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>  
+#include <time.h>   
+#include <math.h>   
+
+int main() {
+    int i = 1;
+    int n = 1000;
+    double I = 0.0;
+    double r;
+    double a = M_PI;             
+    double b = (3.0 * M_PI) / 2; 
+
+    srand(time(0));
+
+    do {
+        r = (b - a) * ((double)rand() / RAND_MAX) + a;
+        
+        I = I + sin(r) * (b - a);
+        
+        printf("I / i = %f\n", I / i);
+        
+        i = i + 1;
+    } while (i <= n);
+
+    printf("I / n = %f\n", I / n);
+
+    return 0;
+}
